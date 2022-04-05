8f12b349c47c096397ec13766e61ff7647166f2d
akarkmj <94171497+akarkmj@users.noreply.github.com>
2022-04-05 13:16:46 -0700

    first push

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
+</head>
+<body>
+<div class="upper">
+       <div class="content">
+           
+       <div class="card">
+            <div class="card-left">
+                <div class=""><img src="{{asset('storage/mala akar.jpg')}}"></div>
+            </div>
+            <div class="card-right">
+                <div class="card-title fw-7">want to know more about me? here is my profile:</div>
+                <div class="card-text fw-5">Name: Akar Kawa Majid<br>
+                                            Phone: 0770 070 70 70<br>
+                                            E-mail: akarnamr@gmail.com<br>
+                                            lives in: kurdstan - slemani</div>
+                <div class="card-right-body">
+                    <!-- <div class="card-rb-1">
+                        <img src="https://rvs-article-preview-component.netlify.app/images/avatar-michelle.jpg" alt="">
+                    </div> -->
+                    <div class="card-rb-2">
+                        <div class="card-rb-2-title fw-7">Akar Kawa Majid</div>
+                        <div class="card-rb-2-text fw-5">2022-5-04</div>
+                    </div>
+                    <div class="card-rb-3">
+                        <div class="card-rb-3-inner"></div>
+                        <div class="card-rb-3-inner-before">
+                            <div class="before-1">SHARE</div>
+                            <div class="before-2"><img src="https://rvs-article-preview-component.netlify.app/images/icon-facebook.svg" alt=""></div>
+                            <div class="before-3"><img src="https://rvs-article-preview-component.netlify.app/images/icon-twitter.svg" alt=""></div>
+                            <div class="before-4"><img src="https://rvs-article-preview-component.netlify.app/images/icon-pinterest.svg" alt=""></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+           <!-- <ul>
+               <li>Name: Akar Kawa Majid</li><br>
+               <li>E-mail: akarnamr@gmail.com</li><br>
+               <li>Address: Slemani, Iraq. Mala Dawd St.</li><br>
+               <li>Contact: +(964) 770 070 70 70</li><br>
+           </ul> -->
+       </div>
+    </div>
+</body>
+</html>
